4dde3369ef7c50013d2a83470402dde01c8f2277
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-06-30 20:00:30 -0300

    Project Initialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Body from './Body';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <Body />;
+  }
+}
+
+export default App;
---
+++ src/Body/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cssModule from 'react-css-modules';
+import styles from './Body.css';
+
+const Body = () => (
+  <div>
+    Body
+  </div>
+);
+
+Body.propTypes = {
+
+};
+
+export default cssModule(Body, styles);
