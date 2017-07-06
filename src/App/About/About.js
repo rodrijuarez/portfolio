6ecf98a9753166aa2786f8c1d8938ba7159698b5
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-07-06 10:27:15 -0300

    Enhancement
     - Added transitions to landing
     - Added transitions to about

--- src/App/About/About.js
+++ src/App/About/About.js
@@ -1,76 +1,214 @@
 import React from 'react';
 import styles from './About.css';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class About extends React.Component {
   static propTypes = {};
 
+  constructor(props) {
+    super(props);
+    this.state = { mounted: false };
+  }
+
   componentDidMount() {
+    this.setState({ mounted: true });
     document.title = 'About - Rodrigo Juarez';
   }
 
-  render = () =>
-    <div className={styles.contentSection}>
-      <div className={styles.row}>
-        <h2>RODRIGO JUAREZ</h2>
-        <p className={styles.justify}>
-          I was born and raised in{' '}
-          <span className={styles.bold}>Buenos Aires, Argentina</span>. In a
-          very short period of time I learned the basics and got involved in the
-          tech industry pretty quickly.   At the beginning of my career I spent
-          most of my time working for software factories, where I’ve met a lot
-          of great people and learned a lot from them. I was also fortunate
-          enough to work on <span className={styles.bold}>Bons</span>, a digital
-          agency with a great focus on UX and UI, a place where I’ve also
-          discovered the startup world.   At some point I realised that I wanted
-          to experience that world at first hand, and that’s where I joined{' '}
-          <span className={styles.bold}>Airfy</span>, the company that I work
-           for currently.
-        </p>
+  getMainSection() {
+    if (this.state.mounted) {
+      return (
+        <div>
+          <h2>RODRIGO JUAREZ</h2>
+          <p className={styles.justify}>
+            I was born and raised in{' '}
+            <span className={styles.bold}>Buenos Aires, Argentina</span>. In a
+            very short period of time I learned the basics and got involved in
+            the tech industry pretty quickly.   At the beginning of my career I
+            spent most of my time working for software factories, where I’ve met
+            a lot of great people and learned a lot from them. I was also
+            fortunate enough to work on{' '}
+            <span className={styles.bold}>Bons</span>, a digital agency with a
+            great focus on UX and UI, a place where I’ve also discovered the
+            startup world.   At some point I realised that I wanted to
+            experience that world at first hand, and that’s where I joined{' '}
+            <span className={styles.bold}>Airfy</span>, the company that I work
+             for currently.
+          </p>
+        </div>
+      );
+    }
+  }
+
+  getCareerSection() {
+    if (this.state.mounted) {
+      return (
+        <div>
+          <h3>Career</h3>
+          <p className={styles.justify}>
+            Nowadays I’m focusing on my career as a{' '}
+            <span className={styles.bold}>front end developer</span>, anyways, I
+            must also tell you that in the past I’ve also worked as a{' '}
+            <span className={styles.bold}>full stack developer</span>, so I can
+            fulfill any role.
+          </p>
+        </div>
+      );
+    }
+  }
+
+  getTechnologiesSection() {
+    if (this.state.mounted) {
+      return (
+        <div>
+          <h3>Technologies</h3>
+          <p className={styles.justify}>
+            In the latest years I’ve been focused on building applications
+            around
+            <span className={styles.bold}> Angular</span> and{' '}
+            <span className={styles.bold}>Javascript</span>, but one of my main
+            goals is to get more involved in{' '}
+            <span className={styles.bold}>React</span> and its community, where
+            I’ve only developed some small projects (i.e{' '}
+            <span className={styles.bold}>this site!</span>).
+          </p>
+        </div>
+      );
+    }
+  }
+
+  getDesignSection() {
+    if (this.state.mounted) {
+      return (
+        <div>
+          <h3>Design</h3>
+          <p className={styles.justify}>
+            Lately I’ve been also getting more involved in learning{' '}
+            <span className={styles.bold}>design</span>; reading books,
+            tutorials, practicing, involving with the communities and learning
+            from them.
+          </p>
+        </div>
+      );
+    }
+  }
+
+  getTravelSection() {
+    if (this.state.mounted) {
+      return (
+        <div>
+          <h3>Travel and relocation</h3>
+          <p className={styles.justify}>
+            I’m also planning to move to{' '}
+            <span className={styles.bold}>Europe</span>, the countries that I’m
+            mostly interested in are <span className={styles.bold}>
+              France
+            </span>{' '}
+            (currently learning french) and{' '}
+            <span className={styles.bold}>Germany</span>. So if you got any
+            offer involving relocation to those countries, I’m all ears!
+          </p>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.contentSection}>
+        <CSSTransitionGroup
+          transitionAppear={true}
+          transitionAppearTimeout={1000}
+          transitionName={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+            leave: styles.leave,
+            leaveActive: styles.leaveActive,
+            appear: styles.appear,
+            appearActive: styles.appearActive
+          }}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          component="div"
+          className={styles.row}
+        >
+          {this.getMainSection()}
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          transitionAppear={true}
+          transitionAppearTimeout={1500}
+          transitionName={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+            leave: styles.leave,
+            leaveActive: styles.leaveActive,
+            appear: styles.appear,
+            appearActive: styles.appearActive
+          }}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          component="div"
+          className={styles.row}
+        >
+          {this.getCareerSection()}
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          transitionAppear={true}
+          transitionAppearTimeout={1500}
+          transitionName={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+            leave: styles.leave,
+            leaveActive: styles.leaveActive,
+            appear: styles.appear,
+            appearActive: styles.appearActive
+          }}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          component="div"
+          className={styles.row}
+        >
+          {this.getTechnologiesSection()}
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          transitionAppear={true}
+          transitionAppearTimeout={1500}
+          transitionName={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+            leave: styles.leave,
+            leaveActive: styles.leaveActive,
+            appear: styles.appear,
+            appearActive: styles.appearActive
+          }}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          component="div"
+          className={styles.row}
+        >
+          {this.getDesignSection()}
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          transitionAppear={true}
+          transitionAppearTimeout={1500}
+          transitionName={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+            leave: styles.leave,
+            leaveActive: styles.leaveActive,
+            appear: styles.appear,
+            appearActive: styles.appearActive
+          }}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={500}
+          component="div"
+          className={styles.row}
+        >
+          {this.getTravelSection()}
+        </CSSTransitionGroup>
       </div>
-      <div className={styles.row}>
-        <h3>Career</h3>
-        <p className={styles.justify}>
-          Nowadays I’m focusing on my career as a{' '}
-          <span className={styles.bold}>front end developer</span>, anyways, I
-          must also tell you that in the past I’ve also worked as a{' '}
-          <span className={styles.bold}>full stack developer</span>, so I can
-          fulfill any role.
-        </p>
-      </div>
-      <div className={styles.row}>
-        <h3>Technologies</h3>
-        <p className={styles.justify}>
-          In the latest years I’ve been focused on building applications around
-          <span className={styles.bold}> Angular</span> and{' '}
-          <span className={styles.bold}>Javascript</span>, but one of my main
-          goals is to get more involved in{' '}
-          <span className={styles.bold}>React</span> and its community, where
-          I’ve only developed some small projects (i.e{' '}
-          <span className={styles.bold}>this site!</span>).
-        </p>
-      </div>
-      <div className={styles.row}>
-        <h3>Design</h3>
-        <p className={styles.justify}>
-          Lately I’ve been also getting more involved in learning{' '}
-          <span className={styles.bold}>design</span>; reading books, tutorials,
-          practicing, involving with the communities and learning from them.
-        </p>
-      </div>
-      <div className={styles.row}>
-        <h3>Travel and relocation</h3>
-        <p className={styles.justify}>
-          I’m also planning to move to{' '}
-          <span className={styles.bold}>Europe</span>, the countries that I’m
-          mostly interested in are <span className={styles.bold}>
-            France
-          </span>{' '}
-          (currently learning french) and{' '}
-          <span className={styles.bold}>Germany</span>. So if you got any offer
-          involving relocation to those countries, I’m all ears!
-        </p>
-      </div>
-    </div>;
+    );
+  }
 }
 
 export default About;
--- src/App/Landing/Landing.js
+++ src/App/Landing/Landing.js
@@ -3,25 +3,104 @@ import styles from './Landing.css';
 import Description from '../Description';
 import Name from '../Name';
 import Profession from '../Profession';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class Landing extends React.Component {
   static propTypes = {};
 
+  constructor(props) {
+    super(props);
+    this.state = { mounted: false };
+  }
+
   componentDidMount() {
+    this.setState({ mounted: true });
     document.title = 'Home - Rodrigo Juarez';
+  }
+
+  getNameSection() {
+    if (!this.state.mounted) return;
+    return (
+      <div>
+        <Name text="Rodrigo Juarez" />
+      </div>
+    );
+  }
+
+  getProfessionSection() {
+    if (!this.state.mounted) return;
+    return (
+      <div>
+        <Profession />
+      </div>
+    );
+  }
+
+  getDescriptionSection() {
+    if (!this.state.mounted) return;
+    return (
+      <div>
+        <Description />
+      </div>
+    );
   }
 
   render = () =>
     <div className={styles.contentSection}>
-      <div className={styles.row}>
-        <Name text="Rodrigo Juarez" />
-      </div>
-      <div className={styles.row}>
-        <Profession />
-      </div>
-      <div className={styles.row}>
-        <Description />
-      </div>
+      <CSSTransitionGroup
+        transitionAppear={true}
+        transitionAppearTimeout={1000}
+        transitionName={{
+          enter: styles.nameSectionEnter,
+          enterActive: styles.nameSectionEnterActive,
+          leave: styles.nameSectionLeave,
+          leaveActive: styles.nameSectionLeaveActive,
+          appear: styles.nameSectionAppear,
+          appearActive: styles.nameSectionAppearActive
+        }}
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={500}
+        component="div"
+        className={styles.row}
+      >
+        {this.getNameSection()}
+      </CSSTransitionGroup>
+      <CSSTransitionGroup
+        transitionAppear={true}
+        transitionAppearTimeout={1500}
+        transitionName={{
+          enter: styles.professionSectionEnter,
+          enterActive: styles.professionSectionEnterActive,
+          leave: styles.professionSectionLeave,
+          leaveActive: styles.professionSectionLeaveActive,
+          appear: styles.professionSectionAppear,
+          appearActive: styles.professionSectionAppearActive
+        }}
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={500}
+        component="div"
+        className={styles.row}
+      >
+        {this.getProfessionSection()}
+      </CSSTransitionGroup>
+      <CSSTransitionGroup
+        transitionAppear={true}
+        transitionAppearTimeout={2000}
+        transitionName={{
+          enter: styles.descriptionSectionEnter,
+          enterActive: styles.descriptionSectionEnterActive,
+          leave: styles.descriptionSectionLeave,
+          leaveActive: styles.descriptionSectionLeaveActive,
+          appear: styles.descriptionSectionAppear,
+          appearActive: styles.descriptionSectionAppearActive
+        }}
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={500}
+        component="div"
+        className={styles.row}
+      >
+        {this.getDescriptionSection()}
+      </CSSTransitionGroup>
     </div>;
 }
 
